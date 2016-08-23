bcef58e7294832e50fd676c94bf929e76501f91d
data-ops <nathan.peirce@rovicorp.com>
2016-08-23 11:46:42 -0400

    Update test.py

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ def get_lucky(int):
     # This function allows the caller to get a lucky number for testing purposes.
     # This function accepts an integer and returns the text equivalent for 7 and 13 as 'seven' or 'thirteen'; otherwise returns 'unlucky'.
 
-    if string = 7:
+    if int == 7:
         return 'seven'
-    elif string = 13:
+    elif int == 13:
         return 'thirteen'
     else:
         return 'unlucky'
